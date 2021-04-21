c3d2605a51b9ec7826320687791edf15810e6121
khalid saaf <khallid.saaf@gmail.com>
2021-04-21 10:13:19 +0000

    debut de projet react

--- back-end/controllers/contactControl.js
+++ back-end/controllers/contactControl.js
@@ -1,4 +1,9 @@
 const contact= require("../models/contact");
+var nodemailer = require('nodemailer');
+
+
+
+
 
 
 module.exports = {
@@ -20,3 +25,30 @@ module.exports = {
         .catch((err) => res.json(err).status(500).end());
     },
 };
+
+
+
+
+
+      var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: 'khaliid.saaf@gmail.com',
+          pass: 'youcode+2020'
+        }
+      });
+
+      var mailOptions = {
+        from: 'khaliid.saaf@gmail.com',
+        to: 'dounia.lasfar1@gmail.com',
+        subject: 'Test Email',
+        text: 'bonjour Dounia ca va'
+      };
+
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log(' Email sent: ' + info.response);
+        }
+      });
